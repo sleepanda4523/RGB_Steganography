a2e4ee7a10a828fa327cc0facc68d12bd894194f
sleepanda <jusuck4523@gmail.com>
2021-12-13 16:45:18 +0900

    EDIT rgb_png

--- sourse/Main_decrypt.py
+++ sourse/Main_decrypt.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
-from sys import exit
-from os import system
 
 """-------------------RGB코드 추출---------------------"""
+
 def rgbopen(path): # 사진 RGB로 열기, 픽셀 = 0,0
-    try : 
-        img = Image.open(str(path))
-    except FileNotFoundError:
-        print("파일이 없습니다")
-        exit()
-    else : 
-        img_RGB = img.convert("RGB")
-        return img_RGB
-    
+    img = Image.open(str(path))
+    img_RGB = img.convert("RGB")
+    return img_RGB
 
 def rgbprint(img, x,y): # RGB 코드 단일 추출
     RGBvalue = img.getpixel((x,y))
@@ -24,8 +17,9 @@ def rgbprint(img, x,y): # RGB 코드 단일 추출
 
 def rgbtxt(img):
     file = ""
-    for y in range(0, img.size[1]):
-        for x in range(0, img.size[0]):
+    print(img.size[0],img.size[1])
+    for x in range(0, img.size[0]):
+        for y in range(0, img.size[1]):
             RGBvalue = img.getpixel((x,y))
             RGBcode = '#'
             for i in range(3):
@@ -40,28 +34,28 @@ def sendRGB(filepath):
     return rgbcode
 """-------------------RSA 복호화---------------------"""
 def decrypt(key,n, ctext):          #복호화
-
     plain = []
-    
-    for char in ctext:
+    length = 2
+    cip_list = [ctext[i:i+length] for i in range(0,len(ctext),length)]
+    for char in cip_list:
         chri = int(char,16)
         pin = hex((chri ** key) % n)
         plain.append(pin[2:])
-    """
-    for i in plain:
-        print(i,end='')
-    """  
+  
     plain_str = ''.join(plain)
     return plain_str
 
 
 if __name__ == "__main__":
-    filepath = input("암호화 된 사진파일 명(확장자포함) : ")
-    private, n = input("개인키와 n : ").split()
-    rgbtext = sendRGB(filepath)
-    ptext = decrypt(private, n, rgbtext)
-    with open("decrypt.txt","wb") as file:
-        file.write(ptext)
+    filepath = "image1.png"     #input("암호화 된 사진파일 명(확장자포함) : ")"""
+    file = open("decrypt.txt","w")
     
+    #private, n = map(int,input("개인키와 n : ").split())
+    #last_len = int(input("rgb오차값"))
+    rgbtext = sendRGB(filepath)
+    
+    #rgbtext = rgbtext
+    #ptext = decrypt(private, n, rgbtext)
+   
+    file.write(rgbtext)
     file.close()
-    system("pause")
